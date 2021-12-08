4d6baf5095cf69361a7fb3d5bfb0af395dbd6155
sebas <sebaslpez0519@gmail.com>
2021-12-08 14:39:02 -0500

    Iniciar proyecto

---
+++ README.md
@@ -0,0 +1,15 @@
+# Challenge reactor-core
+
+Dentro del código se encutra un ejemplo para aplicar reactividad con Spring Boot, se cuenta con una herramienta utilitaria para leer archivos CSV, lo que se buscar es aplicar los operadores reactivas para leer este archivo, y se reta para hacer este procedimiento en mongodb.
+
+Leer las pruebas unitarias.
+
+
+# Step by Step
+
+- Transformar un CSV una lista de Stream de Java (Usar operadores basicos)
+- En vez de usar Java Stream aplicamos reactividad (Reactor Core) con trasnformaciones (Usar operadores basicos)
+- Optimizar las consultas y aplicar un servicio web para hacer estos filtros
+- Hacer un proceso de migración en donde tomemos los datos del archivo y lo llevemos a una base datos (MangoDB)
+- Trabajo directamente los desde una base de datos reactiva y objserva la rendimiento
+- Prueba de concurencia con JMeter, donde vamos a colocar 100 hilos de ejeucución para observar los comportamientos de cada servicio
---
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CSVUtilTest {
+
+    @Test
+    void converterData(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        assert list.size() == 18207;
+    }
+
+    @Test
+    void stream_filtrarJugadoresMayoresA35(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Map<String, List<Player>> listFilter = list.parallelStream()
+                .filter(player -> player.age >= 35)
+                .map(player -> {
+                    player.name = player.name.toUpperCase(Locale.ROOT);
+                    return player;
+                })
+                .flatMap(playerA -> list.parallelStream()
+                        .filter(playerB -> playerA.club.equals(playerB.club))
+                )
+                .distinct()
+                .collect(Collectors.groupingBy(Player::getClub));
+
+        assert listFilter.size() == 322;
+    }
+
+
+    @Test
+    void reactive_filtrarJugadoresMayoresA35(){
+        List<Player> list = CsvUtilFile.getPlayers();
+        Flux<Player> listFlux = Flux.fromStream(list.parallelStream()).cache();
+        Mono<Map<String, Collection<Player>>> listFilter = listFlux
+                .filter(player -> player.age >= 35)
+                .map(player -> {
+                    player.name = player.name.toUpperCase(Locale.ROOT);
+                    return player;
+                })
+                .buffer(100)
+                .flatMap(playerA -> listFlux
+                         .filter(playerB -> playerA.stream()
+                                 .anyMatch(a ->  a.club.equals(playerB.club)))
+                )
+                .distinct()
+                .collectMultimap(Player::getClub);
+
+        assert listFilter.block().size() == 322;
+    }
+
+
+
+}
